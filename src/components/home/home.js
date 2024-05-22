db560f9427574d3879b31946c7d30a2db49b0dfb
Narayan sharma <narayan2k01@gmail.com>
2024-05-22 13:43:49 +0530

    first commit

---
+++ src/components/home/home.js
@@ -0,0 +1,201 @@
+
+import './home.css'
+function Home (){
+    return (<>
+   
+    <div className='section1'>
+    <div className='nav'> 
+    <img src='https://qodematrix.com/wp-content/uploads/2023/10/MotionArtEffect-logo.png'>
+    </img>
+    <button className='nav-btn'>Purchase now</button>
+    </div>
+    <div className="section2">
+        <div className="leftchild">
+            <span>Transform Your Website</span>
+            <p>With Motion Art Effect</p>
+        </div>
+        <div className="rightchild">
+            <h1>Attract Your Visitors Attention With Colorful</h1>
+            <span>Motion Art Effect</span>
+            <p>Unleash the power of creativity with Motion Art for Elementor - your ultimate
+                solution for seamlessly integrating captivating animations into your website.
+            </p>
+        </div>
+    </div>
+        <div className="section3">
+            <h2> Trusted by thousands of users around the world</h2>
+            <div className="section3-rating">
+            <div className="section3-rating1">
+                <div className="media-icon">
+                <img src='https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img2.png'>
+                 </img>
+                </div>
+                <div className="rating-star">
+                    <div className="star">
+                    <img src='https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img4.png'>
+                 </img>
+                    </div>
+                        <p>4.5 Score, 9 Reviews</p>
+                    </div>
+                </div>
+            
+            <div className="section3-rating2">
+                <div className="media-icon">
+                <img src='https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img1.png'>
+                 </img>
+                </div>
+                <div className="rating-star">
+                    <div className="star">
+                    <img src='https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img4.png'>
+                 </img>
+                    </div>
+                    <p>4.5 Score, 9 Reviews</p>
+                    </div>
+                </div>
+            
+            <div className="section3-rating3">
+                <div className="media-icon">
+                <img src='https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img3.png'>
+                 </img>
+                </div>
+                <div className="rating-star">
+                    <div className="star">
+                    <img src='https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img4.png'>
+                 </img>
+                    </div>
+                    <p>4.5 Score, 9 Reviews</p>
+                    </div>
+                </div></div>
+            </div>
+
+            <div className="section4">
+                <div className="section4-leftchild">
+                    <div className="content">
+                    <h2>Turn Your Cursor Into A Colorful Magic Wand & Charm Your Visitors </h2>
+                    <p> Motion Art for Elementor is a groundbreaking plugin that empowers you to effortlessly infuse your website with visually stunning motion art elements.
+
+                    </p></div>
+                    <button className='btn'>Purchase From the site</button>
+                </div>
+                <div className="section4-rightchild">
+                    <img src='https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img5.png'></img>
+                </div>
+            </div>
+            <div className="section5">
+                <h2> Apply On Any Section Or Enable For Whole Page</h2>
+                
+
+
+
+
+<div class="sec5-component">
+    <div class="apply-section">
+        <div class="section-content">
+            <p class="title">Apply On Section</p>
+            <p class="description">Apply on section is a game-changer, offering an unparalleled way to manage applications directly from your website.</p>
+        </div>
+        <div className="section-img-container">
+            <img src='https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img11.png'></img>
+        </div>
+    </div>
+    <div class="apply-page">
+        <div class="section-content">
+            <p class="title">Apply On Page</p>
+            <p class="description">Take your website to new heights with Motion Art for Elementor. Embrace the power of motion and animation.</p>
+        </div>
+        <div className="page-img-container">
+            <img src='https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img10.png'></img>
+        </div>
+    </div>
+            </div>
+
+         <div className="section6">
+            <div className="sec6-container">
+            <h2>
+            Supported by All Popular Browsers
+            </h2>
+                <div className="description">
+                    <p>
+                    Rest assured, Motion Art is designed to be compatible with all major web browsers.
+                    </p>
+                </div>
+                <div className="ctn-browser">
+                <img src='https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img8.png'></img>
+                </div>
+
+            </div>
+            </div> 
+            <div className="section7">
+                
+                    <div className="sec7-upper-content">
+                    <h2>
+                    An All-Round Plugin With Powerful Features
+                    </h2>
+                    <p>
+                    Whether you're a seasoned web designer or just starting out, Motion Art for Elementor seamlessly integrates with the Elementor platform,
+                    providing you with a seamless and intuitive experience.
+                    </p>
+                    </div>
+                    <div className="sec7-lower-content">
+                    <div className="card1">
+                            <img src='https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img9.png'></img>
+                            <div className="card1-ctn">
+                            <h2> Light Weight</h2>
+                            <p>Motion Art for Elementor is designed to be lightweight.</p>
+                            </div>
+                    </div>
+                    <div className="card2">
+                    <img src='https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img6.png'></img>
+                            <div className="card2-ctn">
+                            <h2> 100% Responsive</h2>
+                            <p>Create a consistent visual experience across all devices.</p>
+                            </div>
+                    </div>
+                    <div className="card3">
+                    <img src='https://qodematrix.com/wp-content/uploads/2023/10/motionarteffect-img7.png'></img>
+                            <div className="card3-ctn">
+                            <h2>User Friendly Interface</h2>
+                            <p>Ensure a smooth experience for both applicants and administrators.</p>
+                            </div>
+                    </div>
+                    
+                </div>
+                </div>    
+                <div className="section8"> 
+                
+                <div className="footer-left">
+                <p>© 2023 Copywrite. All rights reserved by QodeMatrix</p>
+
+
+                </div>
+                <div className="footer-right">
+                <ul>
+                   <li>
+                   <a href="https://qodematrix.com/docs/motion-art-for-elementor/" target="_blank">
+                    <span class="elementor-icon-list-text">Documentation</span>
+                    </a>
+                   </li>
+                   <li>
+                   <a href="https://qodematrix.com/docs/motion-art-for-elementor/">
+                    <span class="elementor-icon-list-text">Support</span>
+                    </a>
+                   </li>
+                </ul>
+                
+                </div></div> 
+
+
+
+
+
+
+
+            </div>
+       </div>
+
+
+
+      
+    </>)
+}
+export default Home ;
